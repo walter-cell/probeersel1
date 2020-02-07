b500b16277dda19ee58d43ad581afa8980f921ea
walter-cell <60644051+walter-cell@users.noreply.github.com>
2020-02-07 14:07:00 +0100

    Add files via upload

---
+++ Muzikale_Thermometer/Muzikale_Thermometer.ino
@@ -0,0 +1,197 @@
+
+const int KPinTemp = A0;
+float temperatureC = 0;
+const int kPinSpeaker = 7;
+const int buzzer = 7; 
+const int songspeed = 1.5;
+const int songspeed2 = 2;
+#define NOTE_C4  262
+#define NOTE_D4  294
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_G4  392
+#define NOTE_A4  440
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_D5  587
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_G5  784
+#define NOTE_A5  880
+#define NOTE_B5  988
+
+
+int notesLied1[] = {
+   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
+   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+   NOTE_A4, NOTE_G4, NOTE_A4, 0,
+   
+   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
+   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+   NOTE_A4, NOTE_G4, NOTE_A4, 0,
+   
+   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
+   NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0, 
+   NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
+   NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
+   
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
+   NOTE_D5, NOTE_E5, NOTE_A4, 0, 
+   NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
+   NOTE_C5, NOTE_A4, NOTE_B4, 0,
+
+   NOTE_A4, NOTE_A4, 
+   //Repeat of first part
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
+   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+   NOTE_A4, NOTE_G4, NOTE_A4, 0,
+
+   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
+   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+   NOTE_A4, NOTE_G4, NOTE_A4, 0,
+   
+   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
+   NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0, 
+   NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
+   NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
+   
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
+   NOTE_D5, NOTE_E5, NOTE_A4, 0, 
+   NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
+   NOTE_C5, NOTE_A4, NOTE_B4, 0,
+   //End of Repeat
+
+   NOTE_E5, 0, 0, NOTE_F5, 0, 0,
+   NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
+   NOTE_D5, 0, 0, NOTE_C5, 0, 0,
+   NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4,
+
+   NOTE_E5, 0, 0, NOTE_F5, 0, 0,
+   NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
+   NOTE_D5, 0, 0, NOTE_C5, 0, 0,
+   NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4
+};
+//*****************************************
+int durationLied1[] = {
+  125, 125, 250, 125, 125, 
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 375, 125, 
+  
+  125, 125, 250, 125, 125, 
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 375, 125, 
+  
+  125, 125, 250, 125, 125, 
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 125, 250, 125,
+
+  125, 125, 250, 125, 125, 
+  250, 125, 250, 125, 
+  125, 125, 250, 125, 125,
+  125, 125, 375, 375,
+
+  250, 125,
+  //Rpeat of First Part
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 375, 125, 
+  
+  125, 125, 250, 125, 125, 
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 375, 125, 
+  
+  125, 125, 250, 125, 125, 
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 125, 250, 125,
+
+  125, 125, 250, 125, 125, 
+  250, 125, 250, 125, 
+  125, 125, 250, 125, 125,
+  125, 125, 375, 375,
+  //End of Repeat
+  
+  250, 125, 375, 250, 125, 375,
+  125, 125, 125, 125, 125, 125, 125, 125, 375,
+  250, 125, 375, 250, 125, 375,
+  125, 125, 125, 125, 125, 500,
+
+  250, 125, 375, 250, 125, 375,
+  125, 125, 125, 125, 125, 125, 125, 125, 375,
+  250, 125, 375, 250, 125, 375,
+  125, 125, 125, 125, 125, 500
+};
+int notesLied2[] = {
+   NOTE_C4, NOTE_D4, NOTE_E4, NOTE_C4,  
+   NOTE_C4, NOTE_D4, NOTE_E4, NOTE_C4,
+   NOTE_E4, NOTE_F4, NOTE_G4,
+   NOTE_E4, NOTE_F4, NOTE_G4,
+   NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_C4,
+   NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_C4,
+   NOTE_C4, NOTE_G4, NOTE_C4, NOTE_C4, NOTE_G4, NOTE_C4,
+}; 
+    
+int durationLied2[] = {
+  250, 250, 250, 250, 
+  250, 250, 250, 250,
+  250, 250, 500,
+  250, 250, 500, 
+  
+  125, 125, 125, 125, 250, 250,
+  125, 125, 125, 125, 250, 250,
+  125, 125, 250,
+  125, 125, 250, 
+};  
+//*******************************************************************************//
+void setup() {
+  Serial.begin(9600);
+  pinMode(kPinSpeaker, OUTPUT);
+}
+//*******************************************************************************//
+void loop() {
+  //temperature in Celsius
+  float temperatureC = getTemperatureC();
+
+  Serial.print(temperatureC);
+  Serial.println(" degrees C");
+
+  if(temperatureC < 25){
+    Serial.println("Lied 1");
+      for (int i=0;i<203;i++){
+        float temperatureC = getTemperatureC();
+          if(temperatureC > 25){
+             break;
+          }
+        int wait = durationLied1[i] * songspeed;
+        tone(buzzer,notesLied1[i],wait);
+        delay(wait);
+      }   
+  }
+
+  if(temperatureC > 25){
+    Serial.println("Lied 2");
+    for (int i=0;i<32;i++){
+    float temperatureC = getTemperatureC();
+      if(temperatureC < 25){
+        break;
+      }
+      int wait = durationLied2[i] * songspeed2;
+      tone(buzzer,notesLied2[i],wait);
+      delay(wait);} 
+  }
+}
+//*******************************************************************************//
+  float getTemperatureC(){
+    int reading = analogRead(KPinTemp);
+    float voltage = (reading * 5.0) / 1024;
+    return (voltage - 0.5) * 100;
+  }
+
+
